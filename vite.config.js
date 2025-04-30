bf325ffc27d3d98427d0d14e50f16b5e3ac88d01
Li Chun Ngai, Alex <alexcnli@yahoo.com>
2025-05-01 01:19:08 +0200

    update path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hafas-bitmask-calculator/',
-});
+  base: '/hafas-bitmask-calculator/', // this must match your repo name!
+  build: {
+    outDir: 'docs' // build output goes into /docs folder
+  }
+})
